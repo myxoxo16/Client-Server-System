79ad5a1496ac92b91e88e17b25ea7e99fa3bfd00
myxoxo16 <75792049+myxoxo16@users.noreply.github.com>
2024-11-17 20:20:21 -0500

    Added comments

--- client.c
+++ client.c
@@ -10,61 +10,73 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc != 4) // Expect filename, IP-address:port-number, bufSize
+    //Waiting for the filename, IP-address:port-number and bufSize
+    if (argc != 4) 
     {
+        //Print to user and quit
         printf("Usage: %s <filename> <IP-address:port-number> <bufSize>\n", argv[0]);
         return 1;
     }
 
-    // Extract arguments
+    //The command line arguments for buffer size filename and ip port
+    //but the buffer size as argv3 for flexibility
     char *filename = argv[1];
     char *ip_port = argv[2];
-    int bufSize = atoi(argv[3]); // Buffer size
+    int bufSize = atoi(argv[3]); 
 
     if (bufSize <= 0)
     {
+        //Print message to user if the size of the buffer is less or equal to 0
         fprintf(stderr, "Invalid buffer size: %d\n", bufSize);
         return 1;
     }
 
-    // Split IP and port from ip_port (e.g., "127.0.0.1:8080")
+    //Strtok function to split strings to get correct format for the client command line
     char *server_ip = strtok(ip_port, ":");
     char *port_str = strtok(NULL, ":");
     if (!server_ip || !port_str)
     {
+        //Display message to user
         fprintf(stderr, "Invalid IP-address:port-number format: %s\n", argv[2]);
         return 1;
     }
     int port = atoi(port_str);
     if (port <= 0 || port > 65535)
     {
+        //Display message to user and quit
         fprintf(stderr, "Invalid port number: %s\n", port_str);
         return 1;
     }
 
-    // Open the file to send
+    //Open the file and let user know
     printf("Opening file '%s'...\n", filename);
     FILE *file = fopen(filename, "rb");
     if (!file)
     {
+        //Display message to user that it cant open the file and quit
         perror("Cannot open file");
         return 1;
     }
 
-    // Get file size
+    //Get the size of the file
     struct stat st;
     if (stat(filename, &st) != 0)
     {
+        //Let user know that it cannot get the file size and close the 
+        //file
         perror("Cannot get file size");
         fclose(file);
         return 1;
     }
+    //Put the file in bytes format and print message to user
     uint64_t file_size = st.st_size;
     printf("File size: %lu bytes\n", file_size);
 
-    // Create socket
+    //Make socket
     printf("Creating socket...\n");
     int sock = socket(AF_INET, SOCK_STREAM, 0);
+    //If the socket is less than 0, show error message to user and close
+    //the file
     if (sock < 0)
     {
         perror("Socket creation failed");
@@ -72,13 +84,17 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // Configure server address
+    //Set up the server address with IPv4 and
+    //Convert port number
     struct sockaddr_in server_addr;
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(port);
 
+    //Print message to user
     printf("Parsing server IP address '%s'...\n", server_ip);
+
+    
     if (inet_pton(AF_INET, server_ip, &server_addr.sin_addr) <= 0)
     {
         fprintf(stderr, "Invalid server IP address format: %s\n", server_ip);
@@ -87,7 +103,8 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // Connect to server
+    //Tell the user that its trying to connect to the server and 
+    //display the server ip and port
     printf("Connecting to server %s:%d...\n", server_ip, port);
     if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
     {
@@ -98,9 +115,11 @@ int main(int argc, char *argv[])
     }
     printf("Connected to server %s:%d\n", server_ip, port);
 
-    // Send filename length
+    //Send the length of the filename
     uint32_t name_len = strlen(filename);
-    name_len = htonl(name_len); // Convert to network byte order
+    name_len = htonl(name_len); 
+
+    
     if (send(sock, &name_len, sizeof(name_len), 0) < 0)
     {
         perror("Error sending filename length");
@@ -109,7 +128,7 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // Send filename
+    //Send the filename, display error message otherwise
     if (send(sock, filename, strlen(filename), 0) < 0)
     {
         perror("Error sending filename");
@@ -118,8 +137,8 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // Send file size
-    uint64_t net_file_size = htobe64(file_size); // Convert to network byte order
+    //Send the size of the file
+    uint64_t net_file_size = htobe64(file_size); 
     if (send(sock, &net_file_size, sizeof(net_file_size), 0) < 0)
     {
         perror("Error sending file size");
@@ -128,11 +147,12 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // Send file content
+    //Send the file contents into many chunks
     printf("Sending file data...\n");
     char *buffer = malloc(bufSize);
     if (!buffer)
     {
+        //Print error message to user otherwise and close 
         fprintf(stderr, "Failed to allocate buffer of size %d\n", bufSize);
         close(sock);
         fclose(file);
@@ -151,9 +171,12 @@ int main(int argc, char *argv[])
             return 1;
         }
     }
-
+    
+    //Tell the user that the file is sent successfully and print the exact file
     printf("File '%s' sent successfully (%lu bytes)\n", filename, file_size);
 
+    //Close the file, free the memory and close the socket
+    //Finally, return 0
     free(buffer);
     fclose(file);
     close(sock);
